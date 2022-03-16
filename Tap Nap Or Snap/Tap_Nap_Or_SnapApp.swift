1b6031a5eb8c2d0ccaf02da1acd78d8c80807130
inti <inti.resende@gmail.com>
2022-03-16 17:57:45 +1300

    Initial Commit

---
+++ Tap Nap Or Snap/Tap_Nap_Or_SnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_Nap_Or_SnapApp.swift
+//  Tap Nap Or Snap
+//
+//  Created by Inti Albuquerque on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Tap_Nap_Or_SnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
